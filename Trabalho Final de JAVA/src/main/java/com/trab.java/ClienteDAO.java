6252a3c575d205c619d530929ccbf42b4c69b08b
AlyssonMychael <alysson23091@gmail.com>
2024-11-06 02:20:44 -0300

    Add files via upload

--- Trabalho Final de JAVA/src/main/java/com/trab.java/ClienteDAO.java
+++ Trabalho Final de JAVA/src/main/java/com/trab.java/ClienteDAO.java
@@ -10,7 +10,7 @@ public class ClienteDAO {
 
     public ClienteDAO() {
         try {
-            String url = "jdbc:sqlite:cliente.db";
+            String url = "jdbc:sqlite:clientes.db";
             conexao = DriverManager.getConnection(url);
             criarTabelaClientes();
         } catch (SQLException e) {
